9b27aca63bc937c25a2c7b10dc749bc9c438fab1
Richard Morgan <rpm73@hotmail.com>
2017-08-28 23:18:31 +0800

    Too many changes. Put all the datafiles in gitignore so should just have the source now.

--- get_data.php
+++ get_data.php
@@ -1,5 +1,5 @@
 <?php 
-	$fpath = "datafiles/" . $_GET["fname"];
+	$fpath = "datafiles/" . $_GET["lang"] . "/" . $_GET["fname"];
 	mb_internal_encoding("utf-8");
 	$line = file_get_contents($fpath);
 	echo json_encode($line, JSON_UNESCAPED_UNICODE);
--- get_files.php
+++ get_files.php
@@ -1,5 +1,8 @@
 <?php
-	$dir = "datafiles";
+	$logfile = fopen("get_filesLog.txt","w");
+	$dir = "datafiles/" . $_GET["lang"];
+	fwrite($logfile,"Directory is " . $dir . "\n");
 	$files1 = scandir($dir);
 	echo json_encode($files1);
+	fclose($logfile);
 ?>
